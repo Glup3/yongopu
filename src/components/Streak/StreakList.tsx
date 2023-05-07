5e4dacb875a37708366adc51f030f549a59488c1
Phuc Tran <glup3.tran@gmail.com>
2023-05-07 15:49:25 +0200

    animate streak list

--- src/components/Streak/StreakList.tsx
+++ src/components/Streak/StreakList.tsx
@@ -1,17 +1,19 @@
 import React from "react";
 import { type RouterOutputs } from "../../utils/trpc";
 import { StreakListItem } from "./StreakListItem";
+import { useAutoAnimate } from "@formkit/auto-animate/react";
 
 type Props = {
   streaks: RouterOutputs["streak"]["getUserStreaks"];
 };
 
 export const StreakList: React.FC<Props> = ({ streaks }) => {
+  const [parent] = useAutoAnimate<HTMLUListElement>();
   const allStreaks = [...streaks.values()];
 
   return (
     <div>
-      <ul>
+      <ul ref={parent}>
         {allStreaks.map((streak) => {
           return (
             <li key={streak.id}>
